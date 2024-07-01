a2010981075cb30ac7e19ce3f15b48dfd1906c75
thegreatk <102196225+The-Great-K@users.noreply.github.com>
2024-06-30 22:51:59 -0600

    Created custom compass (doesn't track)

--- src/main/java/com/fadingdaze/playerbounties/PlayerBounties.java
+++ src/main/java/com/fadingdaze/playerbounties/PlayerBounties.java
@@ -1,5 +1,6 @@
 package com.fadingdaze.playerbounties;
 
+import com.fadingdaze.playerbounties.commands.GetTrackingCompass;
 import com.fadingdaze.playerbounties.commands.StartBounty;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -13,18 +14,19 @@ public final class PlayerBounties extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         Bukkit.getPluginManager().registerEvents(this, this);
-        getLogger().info("""
-                            ______  _                           ______                       _    _           \s
-                            | ___ \\| |                          | ___ \\                     | |  (_)          \s
-                            | |_/ /| |  __ _  _   _   ___  _ __ | |_/ /  ___   _   _  _ __  | |_  _   ___  ___\s
-                            |  __/ | | / _` || | | | / _ \\| '__|| ___ \\ / _ \\ | | | || '_ \\ | __|| | / _ \\/ __|
-                            | |    | || (_| || |_| ||  __/| |   | |_/ /| (_) || |_| || | | || |_ | ||  __/\\__ \\
-                            \\_|    |_| \\__,_| \\__, | \\___||_|   \\____/  \\___/  \\__,_||_| |_| \\__||_| \\___||___/
-                                               __/ |                                                          \s
-                                              |___/         \s""");
+//        getLogger().info("""
+//                            ______  _                           ______                       _    _           \s
+//                            | ___ \\| |                          | ___ \\                     | |  (_)          \s
+//                            | |_/ /| |  __ _  _   _   ___  _ __ | |_/ /  ___   _   _  _ __  | |_  _   ___  ___\s
+//                            |  __/ | | / _` || | | | / _ \\| '__|| ___ \\ / _ \\ | | | || '_ \\ | __|| | / _ \\/ __|
+//                            | |    | || (_| || |_| ||  __/| |   | |_/ /| (_) || |_| || | | || |_ | ||  __/\\__ \\
+//                            \\_|    |_| \\__,_| \\__, | \\___||_|   \\____/  \\___/  \\__,_||_| |_| \\__||_| \\___||___/
+//                                               __/ |                                                          \s
+//                                              |___/         \s""");
         getLogger().info("Player Bounties plugin successfully loaded!");
 
         getCommand("startbounty").setExecutor(new StartBounty());
+        getCommand("gettracker").setExecutor(new GetTrackingCompass());
     }
 
     @Override
--- src/main/java/com/fadingdaze/playerbounties/commands/GetTrackingCompass.java
+++ src/main/java/com/fadingdaze/playerbounties/commands/GetTrackingCompass.java
@@ -1,13 +1,35 @@
 package com.fadingdaze.playerbounties.commands;
 
+import com.fadingdaze.playerbounties.PlayerBounties;
+import net.kyori.adventure.text.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 public class GetTrackingCompass implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
+                             @NotNull String name, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("ERROR: Only players can use this command!");
+            return false;
+        }
+
+        ItemStack trackingCompass = new ItemStack(Material.COMPASS);
+        ItemMeta meta = trackingCompass.getItemMeta();
+
+        meta.setDisplayName(ChatColor.BLUE + "Tracking: " + ChatColor.RED + PlayerBounties.bountyHead.getName());
+
+        trackingCompass.setItemMeta(meta);
+
+        ((Player) sender).getInventory().addItem(trackingCompass);
+
         return false;
     }
 }
